647e419e443eb4b0ee5374f83756d8e36c4cb7f9
Dhanushragav003 <dhanushragav003@gmail.com>
2024-11-20 11:57:52 +0530

    script added

---
+++ scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if cowsay is installed
+if ! command -v cowsay &> /dev/null; then
+    echo "cowsay is not installed. Installing it now..."
+    sudo apt update && sudo apt install -y cowsay
+fi
+
+cowsay -f dragon "I'm dragon" >> drago.txt
+
